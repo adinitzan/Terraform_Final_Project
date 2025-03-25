c05ca2406cd4c8d53642096de9b73419b4c35f3f
toharbarazi <tohar.barazi@nitaznim.tech>
2025-03-25 11:52:39 +0200

    iam.role for node group

--- Terraform/eks/main.tf
+++ Terraform/eks/main.tf
@@ -11,12 +11,44 @@ resource "aws_eks_cluster" "eks_AT" {
   })
 }
 
+# IAM Role for EKS Node Group
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks-node-group-role-AT"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach required policies
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 # Add the IAM role to EKS worker nodes so they can pull from ECR
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_AT.name
   node_group_name = "eks-node-group-AT"
   #role_arn   = var.role_arn
-  node_role_arn = var.node_role_arn
+  node_role_arn = aws_iam_role.eks_node_group_role.arn
 
   subnet_ids      = var.subnet_ids
   scaling_config {
@@ -31,7 +63,11 @@ resource "aws_eks_node_group" "eks_node_group" {
     "Name" = "EKS-Node-Group-AT"
   })
 
-  depends_on = [var.node_role_arn]  # Ensure role is created before node group
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_container_registry_policy
+  ]
 }
 
 
